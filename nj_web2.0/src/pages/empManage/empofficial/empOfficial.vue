56e9db98889e4e9a40fa78d1a1915c1d059bc138
tony.chen <tony.chen@DESKTOP-TLQP1V4>
2019-03-28 19:30:55 +0800

    1hhh

--- nj_web2.0/src/pages/empManage/empofficial/empOfficial.vue
+++ nj_web2.0/src/pages/empManage/empofficial/empOfficial.vue
@@ -2,77 +2,172 @@
   <div class="table">
     <Row>
       <Col span="24">
-      <card>
-        <p slot="title">
-          <Icon type="mouse"></Icon>
-          &nbsp;demo测试
-        </p>
-        <Row>
-          <Input :placeholder="$t('输入员工姓名')" style="width: 200px" v-model="orgDemocode"/>
-          <Input :placeholder="$t('输入证件号码')" style="width: 200px" v-model="orgDemoname"/>
-          <Dropdown>
-            <Button type="primary">
-              {{select}}
-              <Icon type="arrow-down-b"></Icon>
-            </Button>
-            <DropdownMenu slot="list" >
-              <span v-for="(item,index) in Salhdstatus" :key="index" @click="selected(item.paramCode,item.paramInfoCn)">
-               <DropdownItem>{{item.paramInfoCn}}</DropdownItem>
-            </span>
-            </DropdownMenu>
-          </Dropdown>
-          <span style="margin: 0;"><Button type="primary" icon="search" @click="getData(1)">{{$t('button.ser')}}</Button></span>
-          <Button type="primary" @click="openUp(NaN,$t('button.add'))">{{$t('button.add')}}</Button>
-          <Button type="error" @click="deletemsg">{{$t('button.del')}}</Button>
-        </Row>
-        <row class="table-form" ref="table-form">
-          <Table @on-select="selectedtable" @on-select-cancel="selectedtable" @on-select-all="selectedtable" @on-sort-change="sortable" :height="tableheight" size="small" border ref="selection" :columns="columns" :data="data"></Table>
-        </row>
-        <Row style="display: flex">          <Page :total="total" :current="page" size="small" show-elevator show-sizer placement="top" @on-page-size-change="sizeChange" @on-change="pageChange":page-size-opts = "[10, 20, 50, 100]" ></Page><Button type="ghost" size="small" shape="circle" icon="refresh" style="margin-left: 20px;display: inline-block;" @click="getData(1)"></Button></Row>
-      </card>
+        <card>
+          <p slot="title">
+            <Icon type="mouse"></Icon>
+            &nbsp;员工转正管理
+          </p>
+          <Row>
+            <Col span="18"
+                 style="width: 100% !important">
+              <Row>
+                <Input placeholder="请输入员工姓名"
+                       style="width: 200px"
+                       v-model="empIdName"/>
+                <Input placeholder="请输入证件号码"
+                       style="width: 200px"
+                       v-model="empIdIden"/>
+                <!--状态选择框-->
+                <template>
+                  <Select v-model="state" clearable style="width:200px" @on-change="getData()">
+                    <Option v-for="item in StateCode" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                  </Select>
+                </template>
+                <span style="margin: 0;">
+                          <Button type="primary" icon="search" @click="search()">查询</Button>
+                        </span>
+                <span style="margin: 0;" v-show="state ==='01empoff' ? true : false">
+                            <Button type="success" @click="openUp(NaN,$t('button.add'))">新增</Button>
+                        </span>
+                <span style="margin: 0;" v-show="state ==='01empoff' ? true : false">
+                            <Button type="success" @click="deletemsg()">删除</Button>
+                        </span>
+                <span style="margin: 0;" v-show="state ==='01empoff' ? true : false">
+                            <Button type="primary" @click="setState('02empoff')">转正</Button>
+                        </span>
+                <span style="margin: 0;" v-show="state ==='01empoff' ? true : false">
+                            <Button type="error" @click="">导入</Button>
+                        </span>
+                <span style="margin: 0;" v-show="true">
+                            <Button type="success" @click="">导出</Button>
+                        </span>
+              </Row>
+              <!--table-->
+              <row class="table-form"
+                   ref="table-form">
+                <Table @on-select="selectedtable"
+                       @on-select-cancel="selectedtable"
+                       @on-select-all="selectedtable"
+                       @on-sort-change="sortable"
+                       :height="tableheight"
+                       size="small"
+                       border
+                       ref="selection"
+                       :columns="columns"
+                       :data="data">
+                </Table>
+              </row>
+              <!--page-->
+              <Row style="display: flex">
+                <Page :total="total"
+                      size="small"
+                      :current="page"
+                      show-elevator
+                      show-sizer
+                      placement="top"
+                      @on-page-size-change="sizeChange"
+                      @on-change="pageChange"
+                      :page-size-opts="[10, 20, 50, 100]">
+                </Page>
+                <Button type="ghost"
+                        size="small"
+                        shape="circle"
+                        icon="refresh"
+                        style="margin-left: 20px;display: inline-block;"
+                        @click="search()">
+                </Button>
+              </Row>
+            </Col>
+          </Row>
+        </card>
       </Col>
     </Row>
-    <transition name="fade">
-      <update v-show="openUpdate" :id="updateId" :logType="logType" :index="index" @closeUp="closeUp" @getData="addNewArray" @update="updateArray" ref="update"></update>
+    <!--切换-->
+   <transition name="fade">
+      <update v-show="openUpdate"
+              :id="updateId"
+              :logType="logType"
+              :index="index"
+              @closeUp="closeUp"
+              ref="update"></update>
     </transition>
   </div>
 </template>
 <script>
   import update from './addEmpofficial'
-  import { isSuccess } from '../../../lib/util'
-  import { getDataLevelUserLoginNew, getDataLevelUserLogin } from '../../../axios/axios'
+  //import searchOrgframe from '../../../components/searchTable/searchOrgframe'
+  import {isSuccess} from '../../../lib/util'
+  // import { getBtnAuth } from '../../../lib/authorityBtn'
+  import {getDataLevelUserLoginNew, getDataLevelUserLogin} from '../../../axios/axios'
 
-  export default{
+  export default {
     data() {
       return {
-        orgDemocode:null,
-        orgdemoname:null,
+        /*状态code*/
+        StateCode: [
+          {
+            value: '01empoff',
+            label: '待转正'
+          },
+          {
+            value: '02empoff',
+            label: '已转正'
+          }
+        ],
+        openChart: false,
+        loading: true,
         tableheight: document.body.offsetHeight - 280,
         value: '',
         logType: '',
         openUpdate: false,
         updateId: NaN,
         tableselected: [],
+        stateDis: "全部",
+        unitTypeId: NaN,
+        state: '01empoff',
         columns: [
           {
             type: 'selection',
             width: 54,
+            fixed: 'left',
             align: 'center',
           },
           {
-            title: 'orgdemocode',
-            key: 'orgDemocode',
-            sortable: 'id',
+            title: "员工姓名",
+            key: 'empIdName',
+            width: 140,
+            fixed: 'left',
+            sortable: 'empId',
           },
           {
-
-            title:'orgDemoname',
-            key: 'orgDemoname',
+            title: "证件号码",
+            width: 180,
+            key: 'empIdIden',
           },
           {
-            title: '查看',
-            title: '查看',
-
+            title: "部门",
+            width: 140,
+            key: 'deptIdDis',
+            sortable: 'deptId',
+          },
+          {
+            title: "岗位",
+            width: 180,
+            key: 'postIdDis',
+            sortable: 'postId',
+          },
+          {
+            title: "员工类型",
+            key: 'empoffType',
+            width: 140,
+          },
+          {
+            title: "试用期评价",
+            key: 'empoffResult',
+            width: 140,
+          },
+          {
+            title: this.$t('button.opr'),
             key: 'action',
             width: 64,
             fixed: 'right',
@@ -101,35 +196,153 @@
         order: 'desc',
         rows: 10,
         page: 1,
-        funId: '1000'
+        funId: '1000',
+        compFnameCnDis: '',
+        treeid: '',
+        empIdName: '',
+        empIdIden: '',
+        openPick: false,
+        params: {
+          _mt: 'empEmpnh.getPage',
+          sort: 'id',
+          order: 'asc',
+          rows: 10,
+          page: 1,
+          funId: '1',
+          logType: '员工转正查询',
+          data: '{}',
+        },
+        modify: 'false',
+        btnList: {
+          // button_draft: '',    //编辑
+          // button_upd: '',      //修改
+          // button_del: '',      //删除
+          // button_add: '',      //新增
+          // button_save: '',     //保存
+          // button_submit: '',   //提交
+          // button_return: '',   //返回
+          // button_cancel: '',   //取消
+          // button_confirm: '',  //确认
+          // button_invalid: '',  //失效
+          // button_valid: '',    //生效
+          // button_export: '',   //导出
+          // button_import: '',   //导入
+        },
       }
     },
-    computed: {
-
-    },
+    computed: {},
+    /*引入子页面初始化，js不需要*/
     components: {
       update,
     },
     mounted() {
-      this.getData(1)
+      this.getData();
     },
     methods: {
-      getData(page) {
-        const t = this
-        if (page) {
-          this.page = page
+      // 勾选数据方法 无需更改
+      selectedtable(selection) {
+        const newArr = []
+        for (let i = 0; i < selection.length; i++) {
+          newArr.push(selection[i].id)
         }
+        this.tableselected = newArr
+      },
+      // 删除方法 无需更改
+      deletemsg() {
+        const t = this
+        if (t.tableselected.length === 0) {
+          t.$Modal.warning({
+            title: '提示',
+            content: '请至少选择一条数据',
+          })
+        } else {
+          t.$Modal.confirm({
+            title: '提示',
+            content: '请确定删除数据',
+            onOk: () => {
+              getDataLevelUserLogin({
+                // 设置删除mt参数 其余无需更改
+                _mt: 'empEmpofficial.deleteByIds',
+                logType: '删除',
+                delIds: t.tableselected.toString(),
+              }).then((res) => {
+                if (isSuccess(res, t)) {
+                  t.tableselected = []
+                  t.getData()
+                }
+              }).catch(() => {
+                t.$Modal.error({
+                  title: '错误',
+                  content: '网络错误',
+                })
+              })
+            },
+            onCancel: () => {},
+          })
+        }
+      },
 
+      // 删除方法 无需更改
+      setState(data) {
+        const t = this
+        if (t.tableselected.length === 0) {
+          t.$Modal.warning({
+            title: '提示',
+            content: '请至少选择一条数据',
+          })
+        } else {
+          t.$Modal.confirm({
+            title: '提示',
+            content: '请确定转正员工',
+            onOk: () => {
+              getDataLevelUserLogin({
+                // 设置删除mt参数 其余无需更改
+                _mt: 'empEmpofficial.setState',
+                logType: '转正',
+                ids: t.tableselected.toString(),
+                state:data,
+              }).then((res) => {
+                if (isSuccess(res, t)) {
+                  t.tableselected = []
+                  t.getData()
+                }
+              }).catch(() => {
+                t.$Modal.error({
+                  title: '错误',
+                  content: '网络错误',
+                })
+              })
+            },
+            onCancel: () => {},
+          })
+        }
+      },
+
+      /*导入*/
+      import() {
+      },
+      /*导出*/
+      export() {
+      },
+      closeFrame() {
+        const t = this
+        t.openPick = false
+      },//关闭Frame
+      closeChart() {
+        this.openChart = false
+      },//关闭员工转正图
+      getData(id) {
+        const t = this
         const data = {
-          _mt: 'orgDemo.getPage',
+          _mt: 'empEmpofficial.getPage',
           rows: t.rows,
           page: t.page,
           sort: t.sort,
           order: t.order,
-          logType: 'Search',
-          funId: 1000,
-          orgDemocode: t.orgDemocode,
-          orgDemoname: t.orgDemoname
+          logType: '员工转正查询',
+          empIdName: t.empIdName,
+          empIdIden: t.empIdIden,
+          state:t.state,
         }
         for (const dat in data) {
           if (data[dat] === '') {
@@ -138,6 +351,7 @@
         }
         getDataLevelUserLoginNew(data).then((res) => {
           if (isSuccess(res, t)) {
+            // forEach(item )
             t.data = res.data.content[0].rows
             t.total = res.data.content[0].records
           }
@@ -148,116 +362,72 @@
           })
         })
       },
-      addNewArray(res) {
-        const t = this
-        t.data.unshift(res)
-      },
-      updateArray(res) {
-        const t = this
-        t.data.splice(t.index, 1, res)
-      },
       sortable(column) {
         this.sort = column.key
         this.order = column.order
         if (this.order !== 'normal') {
-          this.getData()
+          this.getData(this.treeid)
         } else {
           this.order = 'desc'
         }
-      },
+      },//排序
       sizeChange(size) {
         const t = this
         t.rows = size
-        t.getData()
-      },
+        t.getData(this.treeid)
+      },//分页
       pageChange(page) {
         const t = this
         t.page = page
-        t.getData()
-      },
+        t.getData(this.treeid)
+      },//分页
       selectedtable(selection) {
         const newArr = []
         for (let i = 0; i < selection.length; i++) {
           newArr.push(selection[i].id)
         }
-        this.tableselected = newArr
-      },
-      deletemsg() {
-        const t = this
-        if (t.tableselected.length === 0) {
-          t.$Modal.warning({
-            title: this.$t('reminder.remind'),
-            content: this.$t('reminder.leastone'),
-          })
-        } else {
-        	t.$Modal.confirm({
-        		title: this.$t('reminder.remind'),
-        		content: this.$t('reminder.confirmdelete'),
-        		onOk: () => {
-        			getDataLevelUserLogin({
-          _mt: 'orgDemo.delByIds',
-          funId: '1',
-          logType: this.$t('button.del'),
-          delIds: t.tableselected.toString(),
-        }).then((res) => {
-          if (isSuccess(res, t)) {
-            t.tableselected = []
-            t.getData()
-          }
-        }).catch(() => {
-          t.$Modal.error({
-            title: this.$t('reminder.err'),
-            content: this.$t('reminder.errormessage'),
-          })
-        })
-        		},
-        		onCancel: () => {},
-        	})
-        }
-      },
-      open() {
-        const t = this
-        t.opendialog = true
-      },
+        this.tableselected = newArr.toString()
+      },//列表中选中的item
       openUp(id, logType, index) {
         const t = this
         t.updateId = parseInt(id, 10)
         t.logType = logType
         t.openUpdate = true
         t.index = index
-//        t.$refs.update.getSelect()
         if (logType === this.$t('button.upd')) {
           t.$refs.update.getData(id)
         }
       },
-      close() {
+      /*addNewArray (res) {
         const t = this
-        t.opendialog = false
+        t.data.unshift(res)
+        t.getTree()
       },
+      updateArray (res) {
+        const t = this
+        t.data.splice(t.index, 1, res)
+        t.getTree()
+      },*/
       closeUp() {
         const t = this
         t.openUpdate = false
-        t.$refs.update.formValidate.costCode = ''
-        t.$refs.update.formValidate.costName = ''
-        t.$refs.update.formValidate.validdate = ''
-        t.$refs.update.formValidate.invdate = ''
-        t.$refs.update.formValidate.comment = ''
-        t.$refs.update.formValidate.invreason = ''
+        t.$refs.update.formValidate.empId = ''
+        t.$refs.update.formValidate.empIdName = ''
+        t.$refs.update.formValidate.deptIdDis = ''
+        t.$refs.update.formValidate.postIdDis = ''
+        t.$refs.update.formValidate.empoffResult = ''
+        t.$refs.update.formValidate.empoffDocument = ''
+      },//关闭窗口
+      selected(key, name) {
+        this.select = name
+        this.cityTypeName = key
+        this.getData()
+      },//下拉选中
+      search() {
+        this.treeid = ''
+        this.page = 1
+        this.getData()
       },
     },
   }
 </script>
-<style lang="scss" scoped>
-  .table-form{
-    margin: 10px 0;
-  }
-  .margin-right-10{
-    margin-right: 10px;
-  }
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .2s
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0
-  }
-</style>
