db0687d34d9e422f4a20fddd50b23c2464cb7060
chenyun <cheny@hrocloud.com>
2019-04-08 14:22:23 +0800

    员工入职管理主表页面

--- nj_web2.0/src/pages/empManage/empmaster-manage3/visaAreaBase.vue
+++ nj_web2.0/src/pages/empManage/empmaster-manage3/visaAreaBase.vue
@@ -1,40 +1,57 @@
 ﻿<template>
-  <div class="cover">
-    <div class="box">
-      <row>
-        <i-col class="meau-left" span="5">
-          <Menu :active-name="active" width="auto" @on-select="pageTo">
-            <!--主子表左侧页面布局-->
-            <MenuItem name="option">员工基础信息</MenuItem>
-            <MenuItem name="content" v-show="id">学历信息管理</MenuItem>
-            <MenuItem name="content1" v-show="id">合同信息管理</MenuItem>
-            <MenuItem name="content2" v-show="id">工作经历管理</MenuItem>
-            <MenuItem name="content3" v-show="id">家庭成员管理</MenuItem>
-          </Menu>
-        </i-col>
-        <i-col class="meau-right" span="19">
-          <div class="title">
-            <div class="title-text">
-              <Icon type="mouse" size="16" style="margin-right: 10px;"></Icon>
-              {{logType}}
-            </div>
-            <Button type="text" @click="handleReset">
-              <Icon type="close-round" size="16"></Icon>
-            </Button>
-          </div>
-          <div style="margin-top: 40px;padding: 10px;">
-            <!--主表详细信息页面 visaare为特殊参数一般不传 其余无需变更-->
-            <mOption v-show="option" :logType="logType" ref="option" :id="id"></mOption>
-            <!--子表分页页面 mainid为主表id-->
-            <mContent v-show="content" :logType="logType" ref="content" :mainId="id"></mContent>
+    <div class="cover">
+        <div class="box">
+            <row>
+                <i-col class="meau-left"
+                       span="5">
+                    <Menu :active-name="active"
+                          width="auto"
+                          @on-select="pageTo">
+                        <!--主子表左侧页面布局-->
+                        <MenuItem name="option">员工基础信息</MenuItem>
+                        <MenuItem name="content"
+                                  v-show="id">学历信息管理</MenuItem>
+                        <MenuItem name="content1"
+                                  v-show="id">合同信息管理</MenuItem>
+                        <MenuItem name="content2"
+                                  v-show="id">工作经历管理</MenuItem>
+                        <MenuItem name="content3"
+                                  v-show="id">家庭成员管理</MenuItem>
+                    </Menu>
+                </i-col>
+                <i-col class="meau-right"
+                       span="19">
+                    <div class="title">
+                        <div class="title-text">
+                            <Icon type="mouse"
+                                  size="16"
+                                  style="margin-right: 10px;"></Icon>
+                            {{logType}}
+                        </div>
+                        <Button type="text"
+                                @click="handleReset">
+                            <Icon type="close-round"
+                                  size="16"></Icon>
+                        </Button>
+                    </div>
+                    <div style="margin-top: 40px;padding: 10px;">
+                        <!--主表详细信息页面 visaare为特殊参数一般不传 其余无需变更-->
+                        <mOption v-show="option"
+                                 :logType="logType"
+                                 ref="option"
+                                 :id="id"
+                                 :index="index"
+                                 :modity="modity"></mOption>
+                        <!--子表分页页面 mainid为主表id-->
+                        <!-- <mContent v-show="content" :logType="logType" ref="content" :mainId="id"></mContent>
             <mContent1 v-show="content1" :logType="logType" ref="content1" :mainId="id"></mContent1>
             <mContent2 v-show="content2" :logType="logType" ref="content2" :mainId="id"></mContent2>
-            <mContent3 v-show="content3" :logType="logType" ref="content3" :mainId="id"></mContent3>
-          </div>
-        </i-col>
-      </row>
+            <mContent3 v-show="content3" :logType="logType" ref="content3" :mainId="id"></mContent3> -->
+                    </div>
+                </i-col>
+            </row>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 import mOption from "./jichu/updVisaArea";
@@ -44,160 +61,164 @@ import mContent2 from "./gongzuo/visaAreaDocsList";
 import mContent3 from "./jiating/visaAreaDocsList";
 
 export default {
-  data() {
-    return {
-      //        默认参数 无需变更
-      active: "option",
-      option: true,
-      content: false,
-      content1: false,
-      content2: false,
-      content3: false,
-      //        主表查询单条数据的mt
-      data: {
-        _mt: "empEmpnh.getById"
-      }
-    };
-  },
-  components: {
-    mOption,
-    mContent,
-    mContent1,
-    mContent2,
-    mContent3
-  },
-  props: {
-    //      父页面传递参数  visaare一般不传
-    id: Number,
-    logType: String,
-    index: Number
-  },
-  mounted() {},
-  methods: {
-    //      关闭方法 分别调用本页面 父页面 主表详细信息页面 子表分页的清除方法  无需变更
-    handleReset() {
-      this.clear();
-      this.$refs.option.clear();
-      this.$refs.content.clear();
-      this.$emit("closeUp");
+    data () {
+        return {
+            //        默认参数 无需变更
+            active: "option",
+            option: true,
+            content: false,
+            content1: false,
+            content2: false,
+            content3: false,
+            //        主表查询单条数据的mt
+            data: {
+                _mt: "empEmpnh.getById"
+            },
+            id: this.Id
+        };
     },
-    //      默认方法
-    changeMenu() {
-      this.active = "content";
+    components: {
+        mOption,
+        mContent,
+        mContent1,
+        mContent2,
+        mContent3
     },
-    // 主表信息查询方法 无需变更
-    getOption(id, logType) {
-      this.id = parseInt(id, 10);
-      this.data.id = id;
-      this.data.logType = logType;
-      this.$refs.option.getdata(this.data);
+    props: {
+        //      父页面传递参数  visaare一般不传
+        Id: Number,
+        logType: String,
+        index: Number,
+        modity: String
     },
-    //       根据name分别调用 主表或子表的查询方法 无需变更
-    pageTo(name) {
-      this.option = false;
-      this.content = false;
-      this.content1 = false;
-      this.content2 = false;
-      this.content3 = false;
-      this.active = name;
-      switch (name) {
-        case "option":
-          this.title = "修改";
-          this.$refs.content.clear();
-          break;
-        case "content":
-          this.title = "修改";
-          break;
-        case "content1":
-          this.title = "修改";
-          this.$refs.content.clear();
-          break;
-        case "content2":
-          this.title = "修改";
-          this.$refs.content.clear();
-          break;
-        case "content3":
-          this.title = "修改";
-          this.$refs.content.clear();
-          break;
-      }
-      this[name] = true;
-      if (name !== "option") {
-        this.$refs[name].search();
-      } else {
-        this.getOption(this.id, this.$t("button.upd"));
-      }
+    mounted () {
+        console.log(this.modity, "modity")
     },
-    //      清空方法 初始化本页面参数 无需变更
-    clear() {
-      this.option = true;
-      this.content = false;
-      this.content1 = false;
-      this.content2 = false;
-      this.content3 = false;
-      this.id = NaN;
-      this.active = "option";
+    methods: {
+        //      关闭方法 分别调用本页面 父页面 主表详细信息页面 子表分页的清除方法  无需变更
+        handleReset () {
+            this.clear();
+            this.$refs.option.clear();
+            // this.$refs.content.clear();
+            this.$emit("closeUp");
+        },
+        //      默认方法
+        changeMenu () {
+            this.active = "content";
+        },
+        // 主表信息查询方法 无需变更
+        getOption (id, logType) {
+            this.id = parseInt(id, 10);
+            this.data.id = id;
+            this.data.logType = logType;
+            this.$refs.option.getdata(this.data);
+        },
+        //       根据name分别调用 主表或子表的查询方法 无需变更
+        pageTo (name) {
+            this.option = false;
+            this.content = false;
+            this.content1 = false;
+            this.content2 = false;
+            this.content3 = false;
+            this.active = name;
+            switch (name) {
+                case "option":
+                    this.title = "修改";
+                    this.$refs.content.clear();
+                    break;
+                case "content":
+                    this.title = "修改";
+                    break;
+                case "content1":
+                    this.title = "修改";
+                    this.$refs.content.clear();
+                    break;
+                case "content2":
+                    this.title = "修改";
+                    this.$refs.content.clear();
+                    break;
+                case "content3":
+                    this.title = "修改";
+                    this.$refs.content.clear();
+                    break;
+            }
+            this[name] = true;
+            if (name !== "option") {
+                this.$refs[name].search();
+            } else {
+                this.getOption(this.id, this.$t("button.upd"));
+            }
+        },
+        //      清空方法 初始化本页面参数 无需变更
+        clear () {
+            this.option = true;
+            // this.content = false;
+            // this.content1 = false;
+            // this.content2 = false;
+            // this.content3 = false;
+            // this.id = NaN;
+            // this.active = "option";
+        }
     }
-  }
 };
 </script>
 <style>
 .ivu-menu-vertical.ivu-menu-light:after {
-  background-color: transparent;
+    background-color: transparent;
 }
 </style>
 <style lang="scss" scoped>
 @import "../../../sass/updateAndAdd";
 .header-box {
-  display: flex;
-  position: relative;
-  justify-content: flex-end;
+    display: flex;
+    position: relative;
+    justify-content: flex-end;
 }
 .cover .box {
-  padding: 0;
+    padding: 0;
 }
 .cover .box .form {
-  margin-top: 60px;
+    margin-top: 60px;
 }
 .meau-left {
-  margin-top: 30px;
+    margin-top: 30px;
 }
 .meau-right {
-  position: relative;
-  border-left: 2px solid #efefef;
+    position: relative;
+    border-left: 2px solid #efefef;
 }
 .header-cover {
-  display: none;
-  top: 0;
-  right: 0;
-  color: #fff;
-  border-radius: 50%;
-  position: absolute;
-  width: 150px;
-  height: 150px;
-  background-color: rgba(0, 0, 0, 0.3);
+    display: none;
+    top: 0;
+    right: 0;
+    color: #fff;
+    border-radius: 50%;
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    background-color: rgba(0, 0, 0, 0.3);
 }
 .header-box:hover .header-cover {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 .header-box:hover {
-  cursor: pointer;
+    cursor: pointer;
 }
 .header {
-  height: 150px;
-  width: 150px;
-  border-radius: 50%;
-  overflow: hidden;
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+    overflow: hidden;
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.2s;
+    transition: opacity 0.2s;
 }
 .fade-enter,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
 
