9a10483d359c0fde2c89864096701043cf3cb011
hao kai tao <haokt@hrocloud.com>
2019-06-18 11:58:08 +0800

    hao kai tao <haokt@hrocloud.com>

--- nj_web2.0/src/router/atten.js
+++ nj_web2.0/src/router/atten.js
@@ -1,12 +1,18 @@
 
 //日原始考勤
 import attenCheckinitday from '@/pages/attenManage/attenCheckinitday/attenCheckinitday.vue'
+//月修订考勤管理
+import attenMonthreviMange from '@/pages/attenManage/attenMonthrevi/attenMonthreviList.vue'
 
 export default [
-{
+  {
     path: 'attenCheckinitday',
     name: 'attenCheckinitday',
     component: attenCheckinitday,
-},
-
+  },
+  {
+    path: 'attenMonthreviMange',
+    name: 'attenMonthreviMange',
+    component: attenMonthreviMange,
+  },
 ]
