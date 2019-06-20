2cb06441b1ff34b0433b14257ecb55353e8ff435
eddy <eddy.yang@hrocloud.com>
2019-06-20 15:20:05 +0800

    eddy <eddy.yang@hrocloud.com>

--- nj_web2.0/src/pages/common/onChange/atten_vacation.js
+++ nj_web2.0/src/pages/common/onChange/atten_vacation.js
@@ -2,26 +2,63 @@
  * Created by Aaron on 2019/6/18.
  */
 const atten_vacation = {
+  all_dis(){
+
+  },
   vacSdate(node) {
     atten_vacation.vacSdate_set(this.$parent)
   },
   vacSdate_set(t) {
     //如没勾选假后补办,开始日期不得小于[当天日期+请假天数]
     if (t.valueMap.vacSdate) {
-      if (t.$refs[t.valueMap.vacIsreapply][0].formDataSubmit.vacIsreapply === '0') {
-        let nowDate = new Date() //当前时间
+      if (t.$refs[t.valueMap.vacIsreapply][0].formDataSubmit.vacIsreapply === '0' && t.$refs[t.valueMap.vacDay][0].formDataSubmit.vacDay) {
+        var nowDate = new Date() //当前时间
+        var tsday=t.$refs[t.valueMap.vacDay][0].formDataSubmit.vacDay//请假天数
 
-        let sdate = t.$refs[t.valueMap.vacSdate][0].formDataSubmit.vacSdate //请假开始日期
-        //let nowFt =new Date(sdate).format("yyyy-mm-dd")
-        let tsday=t.$refs[t.valueMap.vacDay][0].formDataSubmit.vacDay//请假天数
-        nowDate = new Date(nowDate).setDate(new Date(nowDate).getDate()+tsday); //可入职当前时间时间戳
-        let edate =new Date(nowDate).format("yyyy-mm-dd")
+        var dayF = parseFloat(tsday);
+        var dayI = parseInt(tsday);
+        if(dayF>dayI){
+          dayI=dayI+1;
+        }
 
-        alert(edate);
-        //if (sdate<edate) {
+        nowDate = nowDate.setDate(nowDate.getDate()+dayI); //当天日期+请假天数
+        var edate =new Date(nowDate).format("yyyy-MM-dd")
 
-       // }
-       }
+
+        let vacSdt = t.$refs[t.valueMap.vacSdate][0].formDataSubmit.vacSdate //请假开始日期
+        var sdate =new Date(vacSdt).format("yyyy-MM-dd")
+
+        var veDate = new Date(vacSdt);
+        var dayall=dayI-1;
+        veDate=veDate.setDate(veDate.getDate()+dayall); //当天日期+请假天数
+        var veDateDis =new Date(veDate).format("yyyy-MM-dd")
+
+        if(sdate<edate){
+          t.$refs[t.valueMap.vacSdate][0].$set(t.$refs[t.valueMap.vacSdate][0].formDataSubmit, 'vacSdate', null)
+          t.$refs[t.valueMap.vacEdate][0].$set(t.$refs[t.valueMap.vacEdate][0].formDataSubmit, 'vacEdate', null)
+        }else{
+          t.$refs[t.valueMap.vacSdate][0].$set(t.$refs[t.valueMap.vacSdate][0].formDataSubmit, 'vacSdate', vacSdt)
+          t.$refs[t.valueMap.vacEdate][0].$set(t.$refs[t.valueMap.vacEdate][0].formDataSubmit, 'vacEdate', veDateDis)
+        }
+       }else if (t.$refs[t.valueMap.vacIsreapply][0].formDataSubmit.vacIsreapply === '1' && t.$refs[t.valueMap.vacDay][0].formDataSubmit.vacDay) {
+          var tsday=t.$refs[t.valueMap.vacDay][0].formDataSubmit.vacDay//请假天数
+
+          var dayF = parseFloat(tsday);
+          var dayI = parseInt(tsday);
+          if(dayF>dayI){
+            dayI=dayI+1;
+          }
+          let vacSdt = t.$refs[t.valueMap.vacSdate][0].formDataSubmit.vacSdate //请假开始日期
+          var sdate =new Date(vacSdt).format("yyyy-MM-dd")
+
+          var veDate = new Date(vacSdt);
+          var dayall=dayI-1;
+          veDate=veDate.setDate(veDate.getDate()+dayall); //当天日期+请假天数
+          var veDateDis =new Date(veDate).format("yyyy-MM-dd")
+
+          t.$refs[t.valueMap.vacSdate][0].$set(t.$refs[t.valueMap.vacSdate][0].formDataSubmit, 'vacSdate', vacSdt)
+          t.$refs[t.valueMap.vacEdate][0].$set(t.$refs[t.valueMap.vacEdate][0].formDataSubmit, 'vacEdate', veDateDis)
+      }
     }
   },
 }
