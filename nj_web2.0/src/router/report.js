965a22e237fea231d15d198e964108fbc68a337b
muy@hrocloud.com <muy@hrocloud.com>
2019-07-03 11:37:08 +0800

    报表路由配置 muy@hrocloud.com <muy@hrocloud.com>

--- nj_web2.0/src/router/report.js
+++ nj_web2.0/src/router/report.js
@@ -6,6 +6,8 @@
 const empReportList = resolve => require(['@/pages/report/empReportList'], resolve)
 //离职员工名单
 const empdimReportList = resolve => require(['@/pages/report/empdimReportList'], resolve)
+//异动员工名单
+const empTransReportList = resolve => require(['@/pages/report/empTransReportList'], resolve)
 
 export default [
   {
@@ -17,5 +19,10 @@ export default [
     path: 'empdimReportList',
     name: 'empdimReportList',
     component: empdimReportList,
+  },
+  {
+    path: 'empTransReportList',
+    name: 'empTransReportList',
+    component: empTransReportList,
   },
 ]
