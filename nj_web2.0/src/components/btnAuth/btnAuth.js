4b5f1d92f5696e6a113e524099228ced85300fb2
tony.chen <tony.chen@DESKTOP-TLQP1V4>
2019-07-18 17:17:25 +0800

    123

--- nj_web2.0/src/components/btnAuth/btnAuth.js
+++ nj_web2.0/src/components/btnAuth/btnAuth.js
@@ -280,6 +280,9 @@ export default {
         case "快速离职":
           name = 'buttonQkleave'
           break
+        case "拒绝":
+          name = 'buttonRefuse'
+          break
       }
       return name;
     },
@@ -390,6 +393,10 @@ export default {
           break
         case "button_order":
           btnName = '面谈预约';
+          bType = 'success';
+          break
+        case "button_refuse":
+          btnName = '拒绝';
           bType = 'success';
           break
       }
